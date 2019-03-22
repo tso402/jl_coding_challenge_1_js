bf0c76b3c4ce7dd8cf911c6b431dea7386c164d8
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-22 13:26:21 +0000

    passing test for 1 minute

--- spec/FormatTimeSpec.js
+++ spec/FormatTimeSpec.js
@@ -9,6 +9,9 @@ describe ('FormatTime', function() {
   it("returns '10 seconds' when time given is 10", function(){
     expect(FormatTime(10)).toEqual('10 seconds')
   })
+  it("returns '1 minute' when time given 60", function(){
+    expect(FormatTime(60)).toEqual('1 minute')
+  })
   it("returns '1 minute and 2 seconds' when time given 62", function(){
     expect(FormatTime(62)).toEqual('1 minute and 2 seconds')
   })
